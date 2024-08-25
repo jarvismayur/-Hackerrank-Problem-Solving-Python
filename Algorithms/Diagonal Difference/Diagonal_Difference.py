bc4c07f268768adf8f23e14312aab878c8157880
Mayur Tembhare <contactmayurtembhare@gmail.com>
2024-08-25 15:06:25 +0530

    Solutons

---
+++ Algorithms/Diagonal Difference/Diagonal_Difference.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    matrix = arr
+    lr_daigonal = sum([matrix[i][i] for i in range(n)])
+    rl_daigonal = sum([matrix[i][n - 1 - i] for i in range(n)])
+    return  abs(rl_daigonal  -lr_daigonal)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
