63f3aee290bf2d9d3737fed0732307ea0123f409
Mayur Tembhare <contactmayurtembhare@gmail.com>
2024-08-25 16:11:40 +0530

    Solutions

---
+++ Python/Print Function/Print_Function.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    if(n >=1 & n <= 20 ):   
+        for a in range(n):
+            print(a+1, end="")
